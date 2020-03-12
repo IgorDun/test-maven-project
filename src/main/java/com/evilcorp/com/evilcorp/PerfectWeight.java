a61afd41e4785b0fcac5501755ef472b102f9179
IgorDun <dunaevigor@mail.ru>
2020-03-12 15:53:08 +0400

    add PerfectWeight

---
+++ src/main/java/com/evilcorp/com/evilcorp/PerfectWeight.java
@@ -0,0 +1,18 @@
+package com.evilcorp.com.evilcorp;
+
+public class PerfectWeight {
+    public static void main(String[] args) {
+        double weight = 71;
+        double height = 173;
+        double pefrectWeight1 = (height - 100) - height*0.1 - 5;
+        double pefrectWeight2 = (height - 100) - height*0.1 + 5;
+        if (weight > pefrectWeight1 && weight < pefrectWeight2) {
+            System.out.println("PerfectWeight");
+        } else {
+            System.out.println("nonPerfectWeight");
+            System.out.println("needWeightForPerfect = " + "from "+ pefrectWeight1 + " to "+ pefrectWeight2);
+        }
+        System.out.println("YouWeight = " + weight);
+
+    }
+}
