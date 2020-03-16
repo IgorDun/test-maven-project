7ac939ef24014f7392aa9fb3bdedc4af262c0767
IgorDun <dunaevigor@mail.ru>
2020-03-16 21:15:22 +0400

    add battleSithsVsJedays

---
+++ src/main/java/com/evilcorp/battleSithsVsJedays.java
@@ -0,0 +1,19 @@
+package com.evilcorp;
+
+public class battleSithsVsJedays {
+    public static void main(String[] args) {
+        final int heightJedi = 1;
+        final int heightSith = 1;
+        final boolean jediHigher = heightJedi > heightSith;
+        final boolean sithHigher = heightJedi < heightSith;
+        final boolean faceToFace = heightJedi == heightSith;
+        final boolean palpatine = false;
+        final boolean obiVanVsDarthMoll = true;
+        if ((jediHigher || sithHigher || (faceToFace && obiVanVsDarthMoll)) && !palpatine) {
+            System.out.println("Jedi will win! Go fight!");
+        } else {
+            System.out.println("Jedi will lost! Run beach!");
+        }
+
+    }
+}
